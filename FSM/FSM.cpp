ccb9e367692328f7f4d863dbe573451d7b7bef5e
jonaritz <jonaritz@gmail.com>
2025-07-18 12:31:58 +0200

    Fix Python3 compatibility and Arduino header

--- FSM/FSM.cpp
+++ FSM/FSM.cpp
@@ -4,7 +4,11 @@
 // 17/10/2011
 //-----------------------------------------------------------
 
-#include "WProgram.h"
+#if defined(ARDUINO) && ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
 #include "FSM.h"
 
 #define DEBUGMODE	0
--- Fast_Acquisition/adquisicion.py
+++ Fast_Acquisition/adquisicion.py
@@ -1,17 +1,27 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+
+"""Simple acquisition script for the Fast_Acquisition example.
+
+This utility reads one byte from ``/dev/ttyUSB0`` at 1 Mbit/s, converts the
+value to millivolts and prints the result.  The original version was written
+for Python 2 and failed under Python 3 due to the ``print`` statement and the
+behaviour of ``serial.read``.
+"""
 
 import serial
-import sys 
+import sys
 
 
-ser = serial.Serial('/dev/ttyUSB0', 1000000);
-while 1:
+ser = serial.Serial("/dev/ttyUSB0", 1000000)
+while True:
     try:
-        n=ser.read();
-        n=ord(n)*5000/255;  #Convierto en mV
-        print n;
+        n = ser.read()
+        if not n:
+            continue
+        n = n[0] * 5000 / 255  # Convierto en mV
+        print(n)
     except KeyboardInterrupt:
-        ser.close();        
+        ser.close()
         sys.exit(0)
 
 
